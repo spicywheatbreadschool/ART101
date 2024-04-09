2cfb12a8e603d87a9e0e5abb39cccd8ecdc659c3
Jonathan Luc <jonathan.luc@sjsu.edu>
2024-04-09 01:36:46 -0700

    More code updates

--- p5_code2/proj2/sketch.js
+++ p5_code2/proj2/sketch.js
@@ -1,47 +1,45 @@
 let theta;
-let recMode = false;
+let recMode = true;
 
-// Params for easy editing
-let initEye = 80;
-let eyeLineRatio = 0.4;
-let eyeThetaAdj = 0;
-let eyeNumBranch;
-let eyeMinLen = 0.5;
 let can;
 
 // Recursive Tree Objects
 let mainBranch;
 let eye1;
+let midBranch;
+
+let frameLim1 = 100;
+let frameLim2 = 420;
+let frameLim3 = 1120;
 
 function setup() {
-  can = createCanvas(710, 400);
-  eyeNumBranch = int(2 * PI / radians(22.5))
-  frameRate(60);
-  frameCount = 60;
-  //frameCount = 300;
-  mainBranch = new recursiveTree(120, 0.8, 0, 2, 2);
-  eye1 = new recursiveTree(initEye, eyeLineRatio, eyeThetaAdj, eyeNumBranch, eyeMinLen);
+  can = createCanvas(1920, 1080);
+  frameRate(.5);
+  frameCount = 99;
+  mainBranch = new recursiveTree(120, 0.8, 2);
+  eye1 = new recursiveTree(80, .4, 0.5);
+  midBranch = new recursiveTree(100, 0.5, 2);
 }
 
 function draw() {
   // Phase 1
-  if(frameCount <= 60) {
+  if(frameCount <= frameLim1) {
     background(0);
 
     // Let's pick an angle 0 to 90 degrees based on the mouse position
-    let a = frameCount;
-    let a2 = frameCount + 15;
+    let a = frameCount / 2;
+    let a2 = frameCount / 2 + 15;
     // Convert it to radians
     theta = radians(a);
     // Start the tree from the bottom of the screen
     translate(width/2,height);
     // Move to the end of that line
-    translate(0,-120);
+    translate(0,-400);
 
     // Main branch
     stroke(255);
     strokeWeight(1);
-    mainBranch.twoBranch(mainBranch.initLine, mainBranch.lineRatio, mainBranch.thetaAdj, mainBranch.numBranches, mainBranch.minLen);
+    mainBranch.twoBranch(mainBranch.initLine);
 
     // "eye" branches
     theta = radians(45 / 2);
@@ -49,73 +47,83 @@ function draw() {
     strokeWeight(4);
 
     translate(-60, -100);
-    eye1.reverseBranch(eye1.initLine, eye1.InitLine2, eye1.lineRatio, eye1.thetaAdj, eye1.numBranch, eye1.minLen);
+    eye1.reverseBranch(eye1.initLine, eye1.InitLine2);
     translate(120, 0);
-    eye1.reverseBranch(eye1.initLine, eye1.InitLine2, eye1.lineRatio, eye1.thetaAdj, eye1.numBranch, eye1.minLen);
-  } else if(frameCount <= 300) {
+    eye1.reverseBranch(eye1.initLine, eye1.InitLine2);
+  } else if(frameCount <= frameLim2) {
     background(0);
 
-    let a = 60 + frameCount / 55;
-    let a2 = 45 / 2 + frameCount - 60;
+    let a = (frameLim1/2) + frameCount / 55;
+    let a2 = 45 / 2 + (frameCount - frameLim1) * 2;
     let rand = random(-frameCount / 30, frameCount / 30);
 
     translate(width/2,height);
     // Start adding a shake to the mainBranch that increases as frame increases
-    translate(0 + rand, -120 + rand);
+    translate(0 + rand, -400 + rand);
     // Main branch but it remains where it left off at frame 60
     stroke(255);
     strokeWeight(1);
     theta = radians(a);
-    mainBranch.twoBranch(mainBranch.initLine, mainBranch.lineRatio, mainBranch.thetaAdj, mainBranch.numBranches, mainBranch.minLen);
+    mainBranch.twoBranch(mainBranch.initLine);
 
     translate(-rand, -rand);
     translate(-60, -100);
     // Eye branches but begin changing theta for them now
     theta = radians(a2);
     stroke(145, 12, 0);
-    strokeWeight(4);
+    strokeWeight(4 - (frameCount - frameLim1) / 105);
 
-    eye1.initEye += (frameCount - 60);
-    eye1.lineRatio += (frameCount / 600);
-    eye1.initLine2 = frameCount - 60;
+    eye1.initLine += (frameCount - frameLim1) / frameLim1;
+    eye1.initLine2 += (frameCount - frameLim1) / frameLim1;
+    //eye1.minLen += 0.02;
 
-    eye1.reverseBranch(eye1.initLine, eye1.initLine2, eye1.lineRatio, eye1.thetaAdj, eye1.numBranch, eye1.minLen);
+    eye1.reverseBranch(eye1.initLine, eye1.initLine2);
     translate(120, 0)
-    eye1.reverseBranch(eye1.initLine, eye1.initLine2, eye1.lineRatio, eye1.thetaAdj, eye1.numBranch, eye1.minLen);
-  } else if(frameCount <= 1000) {
-    /* to do
+    eye1.reverseBranch(eye1.initLine, eye1.initLine2);
+  } else if(frameCount <= frameLim3) {
     background(0);
 
-    let a = frameCount - 300;
-    let a2 = 45 / 2 + frameCount - 60;
-    let rand = random(-frameCount / 30, frameCount / 30);
+    let a = frameLim1/2 + frameLim2 / 55;
+    let a2 = 45 / 2 + (frameCount - frameLim1) * 2;
+    let a3 = radians(9); // Mid branch will have a very slight deviation to have a curve shape (assumedly.)
+    let rand = random(-frameCount / 25, frameCount / 25);
 
     translate(width/2,height);
-    translate(0 + rand, -120 + rand);
-    // Main branch but it remains where it left off at frame 60
+    // Sahke increases in intensity
+    translate(0 + rand, -400 + rand);
+    // Main branch but it remains where it left off at frame 420
     stroke(255);
     strokeWeight(1);
     theta = radians(a);
-    branch(120, 0.8, 0, 2, 2);
+    mainBranch.twoBranch(mainBranch.initLine);
 
     translate(-rand, -rand);
-    // Eye branches but begin changing theta for them now
+    translate(-60, -100);
+    // Render eye branches wherever they end up
     theta = radians(a2);
     stroke(145, 12, 0);
-    strokeWeight(4);
+    strokeWeight(4 - (frameLim2 - frameLim1) / 105);
 
-    translate(-60, -100);
-    branch(initEye + frameCount - 60, eyeLineRatio + frameCount / 600, eyeThetaAdj, int(2 * PI / theta), eyeMinLen);
-    rotate(PI);
-    branch(frameCount - 60, eyeLineRatio, eyeThetaAdj, int(2 * PI / theta), eyeMinLen);
+    eye1.initLine -= (frameCount - frameLim1) / (frameLim2 * 100);
+    eye1.initLine2 -= (frameCount - frameLim1) / (frameLim2 * 100);
+    eye1.reverseBranch(eye1.initLine, eye1.initLine2);
+    translate(120, 0)
+    eye1.reverseBranch(eye1.initLine, eye1.initLine2);
 
+    // Ok here starts the stuff for mid branch
+    translate(-60, -300);
     rotate(PI);
-    translate(120, 0);
-    branch(initEye + frameCount - 60, eyeLineRatio + frameCount / 600, eyeThetaAdj, int(2 * PI / theta), eyeMinLen);
-    rotate(PI);
-    branch(frameCount - 60, eyeLineRatio, eyeThetaAdj, int(2 * PI / theta), eyeMinLen);
-    rotate(PI);
-    */
+    theta = a3;
+    strokeWeight(.5);
+    stroke(0, 0, 0);
+    midBranch.initLine = 100 + (frameCount - frameLim2) * 1.1;
+    midBranch.halfBranch(midBranch.initLine);
+
+    translate(-200, 0);
+    midBranch.halfBranch(midBranch.initLine);
+    translate(400, 0);
+    midBranch.halfBranch(midBranch.initLine);
+
   }
   // Recording functionality
   console.log(frameCount);
@@ -152,72 +160,88 @@ class recursiveTree {
   initLine;
   initLine2;
   lineRatio;
-  thetaAdj;
-  numBranch;
   minLen;
-  constructor(initLine, lineRatio, thetaAdj, numBranch, minLen) {
+  constructor(initLine, lineRatio, minLen) {
     this.initLine = initLine;
     this.initLine2 = 0;
     this.lineRatio = lineRatio;
-    this.thetaAdj = thetaAdj;
-    this.numBranch = numBranch;
     this.minLen = minLen;
   }
-  twoBranch(h, lineRatio, thetaAdj, numBranches, minLen) {
+  halfBranch(h) {
+    h *= this.lineRatio;
+    if (h > this.minLen) {
+      if(h/2 > this.minLen) {
+        push();    
+        rotate(theta);  
+        line(0, 0, 0, -h/2);  
+        translate(0, -h/2); 
+        this.halfBranch(h);   
+        pop();    
+      }
+    
+      push();
+      rotate(-theta);
+      line(0, 0, 0, -h);
+      translate(0, -h);
+      this.halfBranch(h);
+      pop();
+    }
+  }
+  twoBranch(h) {
     // Each branch will be 2/3rds the size of the previous one
-    h *= lineRatio;
-    if (h > minLen) {
+    h *= this.lineRatio;
+    if (h > this.minLen) {
       push();    // Save the current state of transformation (i.e. where are we now)
-      rotate(theta + thetaAdj);   // Rotate by theta
+      rotate(theta);   // Rotate by theta
       line(0, 0, 0, -h);  // Draw the branch
       translate(0, -h); // Move to the end of the branch
-      this.twoBranch(h, lineRatio, thetaAdj, numBranches, minLen);       // Ok, now call myself to draw two new branches!!
+      this.twoBranch(h);       // Ok, now call myself to draw two new branches!!
       pop();     // Whenever we get back here, we "pop" in order to restore the previous matrix state
   
       // Repeat the same thing, only branch off to the "left" this time!
       push();
-      rotate(-theta - thetaAdj);
+      rotate(-theta);
       line(0, 0, 0, -h);
       translate(0, -h);
-      this.twoBranch(h, lineRatio, thetaAdj, numBranches, minLen);
+      this.twoBranch(h);
       pop();
     }
   }
-  reverseBranch(h, h2, lineRatio, thetaAdj, numBranches, minLen) {
+  reverseBranch(h, h2) {
     // Each branch will be 2/3rds the size of the previous one
-    h *= lineRatio;
-    h2 *= lineRatio;
-    if (h > minLen) {
+    h *= this.lineRatio;
+    h2 *= this.lineRatio;
+    if (h > this.minLen) {
       push();    // Save the current state of transformation (i.e. where are we now)
-      rotate(theta + thetaAdj);   // Rotate by theta
+      rotate(theta);   // Rotate by theta
       line(0, 0, 0, -h);  // Draw the branch
       translate(0, -h); // Move to the end of the branch
-      this.reverseBranch(h, h2, lineRatio, thetaAdj, numBranches, minLen);       // Ok, now call myself to draw two new branches!!
+      this.reverseBranch(h, h2);       // Ok, now call myself to draw two new branches!!
       pop();     // Whenever we get back here, we "pop" in order to restore the previous matrix state
       
       // Repeat the same thing, only branch off to the "left" this time!
       push();
-      rotate(-theta - thetaAdj);
+      rotate(-theta);
       line(0, 0, 0, -h);
       translate(0, -h);
-      this.reverseBranch(h, h2, lineRatio, thetaAdj, numBranches, minLen);
+      this.reverseBranch(h, h2);
       pop();
     }
-    if(h2 > minLen) {
+    if(h2 > this.minLen) {
       rotate(PI);
 
       push();    
-      rotate(theta + thetaAdj);  
+      rotate(theta);  
       line(0, 0, 0, -h2);  
       translate(0, -h2); 
-      this.reverseBranch(h, h2,  lineRatio, thetaAdj, numBranches, minLen);      
+      this.reverseBranch(h, h2);      
       pop();     
 
       push();
-      rotate(-theta - thetaAdj);
+      rotate(-theta);
       line(0, 0, 0, -h2);
       translate(0, -h2);
-      this.reverseBranch(h, h2, lineRatio, thetaAdj, numBranches, minLen);
+      this.reverseBranch(h, h2);
       pop();
 
       rotate(PI);
